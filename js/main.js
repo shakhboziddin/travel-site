7e22ffce4866a923dcd8af5a96aafc3845c8f5b4
shahboziddin2007 <shahboziddin2007@gmail.com>
2022-07-07 07:29:54 -0700

    frontend finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,63 @@
+//navbar
+let navBtn = document.querySelector("#navMenu");
+let navbar = document.querySelector(".responsMenu");
+
+window.onscroll = () => {
+    navBtn.classList.remove("fa-times");
+    navbar.classList.remove("active");
+    search.classList.remove("active");
+    form.classList.remove("active");
+    searchBtn.classList.remove("fa-times")
+    btn.classList.remove("fa-times")
+}
+
+navBtn.addEventListener("click", ()=>{
+  navBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+// navbar form
+
+let searchBtn = document.querySelector("#search");
+let search = document.querySelector(".searchForm");
+
+searchBtn.addEventListener("click", () => {
+  searchBtn.classList.toggle("fa-times");
+  search.classList.toggle("active");
+});
+
+// login form
+let btn = document.querySelector("#login");
+let form = document.querySelector(".form");
+let close = document.querySelector("#close");
+
+close.onclick = function(){
+  form.classList.remove("active");
+  btn.classList.remove("fa-times");
+}
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("fa-times");
+  form.classList.toggle("active");
+});
+
+// video controls
+
+let videoBtn = document.querySelectorAll(".control");
+
+videoBtn.forEach(a =>{
+  a.addEventListener("click", ()=>{
+    document.querySelector(".controls .active").classList.remove("active");
+    a.classList.add("active");
+
+
+    let src = a.getAttribute("data-src");
+    document.querySelector("#video").src = src; 
+  });
+});
+
+
+
+// first swiper
 var swiper = new Swiper(".review-cards", {
     spaceBetween: 20,
     loop:true,
@@ -20,6 +80,8 @@ var swiper = new Swiper(".review-cards", {
     },
 });
 
+
+// second swiper
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 20,
     centeredSlides: true,
@@ -31,8 +93,8 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    },
-    breakpoints: {
+        },
+        breakpoints: {
       200: {
         slidesPerView: 2,
       },
